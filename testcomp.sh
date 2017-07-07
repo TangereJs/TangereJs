6d64e2822d5781cc7cfaf3729d41c42a7e8a3349
Ivan Jovanovic <ij.smartnet@gmail.com>
2017-07-07 14:29:38 +0200

    added test component script

---
+++ testcomp.sh
@@ -0,0 +1,17 @@
+compname=$1
+
+#--- validate command line paramters ---
+if test $# -lt 1 ; then
+	echo "Usage: testcomp.sh component-name"
+	exit 1
+fi
+#--- restore repos with "our" code from components.list file ---
+echo "starting component test"
+# this is how you get parent folder path, because unix
+parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+
+cd components
+cd $compname
+ln -sf $parent_path/components /bower_components
+wct
+
