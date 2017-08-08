6165bfe3560dc4b8f1ef05119052445e8b7a9490
Ivan Jovanovic <ij.smartnet@gmail.com>
2017-08-08 10:23:07 +0200

    trying the copy method

--- testcomp.sh
+++ testcomp.sh
@@ -7,11 +7,9 @@ if test $# -lt 1 ; then
 fi
 #--- restore repos with "our" code from components.list file ---
 echo "starting component test"
-# this is how you get parent folder path, because unix
-parent_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 
 cd components
 cd $compname
-ln -sf $parent_path/components /bower_components
+cp -R ../components /bower_components
 wct
 
