2235c010866de4b2eb2463158b66e6126839e253
Ivan Jovanovic <ij.smartnet@gmail.com>
2017-08-17 13:56:43 +0200

    AT-360 testcomp script now detects if test folder exists -- testcomp script now detects if test/index file exists -- testcomp script now correctly executes windows commands (because ln doesn't work) -- kept script code that works on travis-ci

--- testcomp.sh
+++ testcomp.sh
@@ -2,13 +2,37 @@ compname=$1
 
 #--- validate command line paramters ---
 if test $# -lt 1 ; then
-	echo "Usage: testcomp.sh component-name"
-	exit 1
+  echo "Usage: testcomp.sh component-name"
+  exit 1
 fi
 #--- restore repos with "our" code from components.list file ---
-echo "starting component test"
+echo 
+echo 
+echo 
+echo 
+echo "Starting tests for ${compname}"
 
 cd ./components
+build_dir=$(pwd)
+
 cd ./$compname
-ln -s $TRAVIS_BUILD_DIR/components ./bower_components
-xvfb-run wct
+
+if [ ! -d "test" ]; then
+  echo "${compname} component doesn't have tests";
+  exit 1
+fi
+
+if [ ! -e "test/index.html" ]; then
+  echo "${compname} component doesn't have tests";
+  exit 1
+fi
+
+if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
+  win_build_dir=$( echo $build_dir | sed 's/^\///' | sed 's/\//\\/g' | sed 's/^./\0:/' )
+  cmd <<< "mklink /D bower_components ${win_build_dir}"  
+  wct
+  cmd <<< "rmdir bower_components"
+else
+  ln -s $TRAVIS_BUILD_DIR/components ./bower_components
+  xvfb-run wct
+fi
