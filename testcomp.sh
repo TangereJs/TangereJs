d704382cd0b10feff4d03c7bcdf6a6a0ac7a1bed
Ivan Jovanovic <ij.smartnet@gmail.com>
2017-09-07 15:54:59 +0200

    AT-360 added file based logging for passed, failed and missing tests -- updated testcomp so that file logging works locally (win) and on travis-ci (linux)

--- testcomp.sh
+++ testcomp.sh
@@ -6,10 +6,6 @@ if test $# -lt 1 ; then
   exit 1
 fi
 #--- restore repos with "our" code from components.list file ---
-echo 
-echo 
-echo 
-echo 
 echo "Starting tests for ${compname}"
 
 tangerejs_dir=$(pwd)
@@ -20,13 +16,15 @@ build_dir=$(pwd)
 cd ./$compname
 
 if [ ! -d "test" ]; then
-  echo "${compname} component doesn't have tests";
-  exit 1
+  # echo "${compname} component doesn't have tests";
+  echo "${compname}" >> ${tangerejs_dir}/tests_missing.log
+  exit 0
 fi
 
 if [ ! -e "test/index.html" ]; then
-  echo "${compname} component doesn't have tests";
-  exit 1
+  # echo "${compname} component doesn't have tests";
+  echo "${compname}" >> ${tangerejs_dir}/tests_missing.log
+  exit 0
 fi
 
 if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
@@ -35,9 +33,21 @@ if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
   cmd <<< "copy ${win_tangerejs_dir}\win-wct.conf.json wct.conf.json"
   cmd <<< "mklink /D bower_components ${win_build_dir}"  
   wct --skip-selenium-install
+  result=$?
+  if [ $result -eq 0 ]; then
+    echo "${compname}" >> ${tangerejs_dir}/tests_success.log
+  else 
+    echo "${compname}" >> ${tangerejs_dir}/tests_failure.log
+  fi
   cmd <<< "rmdir bower_components"
   cmd <<< "del wct.conf.json"
 else
   ln -s $TRAVIS_BUILD_DIR/components ./bower_components
   xvfb-run wct
+  result=$?
+  if [ $result -eq 0 ]; then
+    echo "${compname}" >> ${tangerejs_dir}/tests_success.log
+  else 
+    echo "${compname}" >> ${tangerejs_dir}/tests_failure.log
+  fi
 fi
