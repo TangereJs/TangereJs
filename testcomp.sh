4255138d85c4e0d54acdc7075bd1f62250054a88
Ivan Jovanovic <ij.smartnet@gmail.com>
2018-05-24 12:01:07 +0200

    attempt #1 to speed up wct startup on linux

--- testcomp.sh
+++ testcomp.sh
@@ -53,7 +53,7 @@ if [ "$(expr substr $(uname -s) 1 10)" == "MINGW64_NT" ]; then
   cmd <<< "del wct.conf.json"
 else
   ln -s $TRAVIS_BUILD_DIR/components ./bower_components
-  xvfb-run -a wct
+  xvfb-run -a wct --skip-selenium-install --skip-update-check --color --skip-plugin BABEL --compile never
   result=$?
   if [ $result -eq 0 ]; then
     echo "${compname}" >> ${tangerejs_dir}/tests_success.log
